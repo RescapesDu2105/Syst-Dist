cbf0841b29e2ad3dc712c88778cf57eb5220c954
Doublon <tussetquentin07@hotmail.com>
2017-12-18 14:44:50 +0100

    BD

---
+++ Projet/BD/Creation_BD.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `bd_systdist`.`patient` (
+  `idPatient` INT NOT NULL,
+  `Nom` VARCHAR(45) NULL,
+  `Prenom` VARCHAR(45) NULL,
+  `Login` VARCHAR(45) NULL,
+  PRIMARY KEY (`idPatient`));
+
+CREATE TABLE `bd_systdist`.`medecin` (
+  `idMedecin` INT NOT NULL,
+  `Nom` VARCHAR(45) NULL,
+  `Prenom` VARCHAR(45) NULL,
+  `Login` VARCHAR(45) NULL,
+  PRIMARY KEY (`idMedecin`));
+
+CREATE TABLE `bd_systdist`.`demande` (
+  `idDemande` INT NOT NULL,
+  `RefPatient` INT NULL,
+  `RefMedecin` INT NULL,
+  `DateHeureDemande` DATETIME NULL,
+  `Urgent` INT NULL,
+  PRIMARY KEY (`idDemande`),
+  CONSTRAINT demande_patient_ref FOREIGN KEY (RefPatient)
+	REFERENCES Patient(idPatient),
+  CONSTRAINT demande_medecin_ref FOREIGN KEY(RefMedecin)
+	REFERENCES Medecin(idMedecin));
+
+CREATE TABLE `bd_systdist`.`analyses` (
+  `idAnalyses` INT NOT NULL,
+  `Item` VARCHAR(45) NULL,
+  `Valeur` VARCHAR(45) NULL,
+  PRIMARY KEY (`idAnalyses`));
+
+CREATE TABLE `bd_systdist`.`logs` (
+  `idLogs` INT NOT NULL,
+  `Infos` VARCHAR(45) NULL,
+  PRIMARY KEY (`idLogs`));
